bfcc76344bc19eb77bdcc16eea0b3473f918f771
gourdo1 <gourdo1@users.noreply.github.com>
2023-04-10 15:01:53 -0700

    workaround for flash/EEPROM size issue preventing compile (config.h)

--- keyboards/gmmk/pro/rev1/ansi/keymaps/gourdo1/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/gourdo1/config.h
@@ -17,6 +17,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+// April 2023 update: QMK supports GMMK Pro by emulating EEPROM through its onboard flash and using a wear-leveling algorithm to spread out writes. Recent QMK changes require me to now explicitly specify a larger EEPROM
+// size to properly fit my firmware (which requires 1195 bytes as of April 10, 2023). In the future, I may need to increase this if firmware grows further. Also, I don't currently explicitly call out EEPROM_DRIVER = wear_leveling and
+// WEAR_LEVELING_DRIVER = embedded_flash in rules.mk but may need to in the future if defaults change -- QMK should eventually support writing to my MCU's actual EEPROM.
+#define WEAR_LEVELING_LOGICAL_SIZE 1280             //default 1024    Number of bytes “exposed” to the rest of QMK and denotes the size of the usable EEPROM.
+#define WEAR_LEVELING_BACKING_SIZE 2560             //default 2048    Number of bytes used by the wear-leveling algorithm for its underlying storage, and needs to be a multiple of the logical size.
+
 // Force n-key rollover
 #define FORCE_NKRO
 
